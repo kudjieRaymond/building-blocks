079b2d167f9edca3a0ac69c68f994e29c672d364
Ray Kudjie <rkudjie@gmail.com>
2019-05-03 11:14:30 +0000

    create my_select method

--- enumerables/enumerable.rb
+++ enumerables/enumerable.rb
@@ -1,6 +1,11 @@
 module Enumerable
 	
 	def my_each
+		if self.class == Hash
+			for
+		else
+		
+		end
 		for item in self
 			yield
 		end
@@ -15,7 +20,35 @@ module Enumerable
 		end
 	end
 
-	
+	def my_select 
+
+		self.class == Array ? arr =[] : arr={}
+
+		self.my_each do |x|
+			arr << x if yield(x)
+		end
+
+		arr
+	end
+
+	def my_all? 
+		self.my_each do |x|
+			#stop if item is flagged
+			return false if yield == false
+		end
+
+		true
+	end
+
+	def my_any? 
+		self.my_each do |x|
+			#stop if any item is true
+			return true if yield == true
+		end
+		
+		false
+	end
+
 
 
 end
